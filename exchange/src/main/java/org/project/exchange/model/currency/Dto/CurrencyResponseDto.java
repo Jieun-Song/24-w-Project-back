d8f8bb66bbe2396dc5040bc0e1c4a86994165595
jieun song <0scarl1216@mju.ac.kr>
2025-03-22 16:49:40 +0900

    fix: fix character error

--- exchange/src/main/java/org/project/exchange/model/currency/Dto/CurrencyResponseDto.java
+++ exchange/src/main/java/org/project/exchange/model/currency/Dto/CurrencyResponseDto.java
@@ -10,7 +10,7 @@ import org.project.exchange.model.currency.Currency;
 @NoArgsConstructor
 @Slf4j
 public class CurrencyResponseDto {
-    private Long currentId;
+    private Long currencyId;
     @JsonProperty("cur_unit")
     private String curUnit;
     @JsonProperty("deal_bas_r")
@@ -19,7 +19,7 @@ public class CurrencyResponseDto {
     private String curNm;
 
     public CurrencyResponseDto(Currency currency) {
-        this.currentId = currency.getCurrencyId();
+        this.currencyId = currency.getCurrencyId();
         this.curUnit = currency.getCurUnit();
         this.dealBasR = String.valueOf(currency.getDealBasR());
         this.curNm = currency.getCurNm();
