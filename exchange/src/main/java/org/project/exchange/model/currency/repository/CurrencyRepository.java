36021c7af7e576dd8f12d09f4d76f80931c41f5d
chaewon02 <glgl246@naver.com>
2025-04-07 21:13:10 +0900

    feat: Currency createAt 추가

--- exchange/src/main/java/org/project/exchange/model/currency/repository/CurrencyRepository.java
+++ exchange/src/main/java/org/project/exchange/model/currency/repository/CurrencyRepository.java
@@ -2,8 +2,11 @@ package org.project.exchange.model.currency.repository;
 
 import org.project.exchange.model.currency.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 
