17e98230eac38d58f803c18571adc84c9f0b3e1a
chaewon02 <glgl246@naver.com>
2025-05-12 15:03:16 +0900

    feat: CreateProductResponseDto 수정

--- exchange/src/main/java/org/project/exchange/model/product/Dto/CreateProductResponseDto.java
+++ exchange/src/main/java/org/project/exchange/model/product/Dto/CreateProductResponseDto.java
@@ -3,17 +3,23 @@ package org.project.exchange.model.product.Dto;
 import lombok.Getter;
 import org.project.exchange.model.product.Product;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class CreateProductResponseDto {
     private Long productId;
     private String name;
     private Double originPrice;
     private Long listId;
+    private Boolean deletedYn;
+    private LocalDateTime createdAt;
 
     public CreateProductResponseDto(Product product) {
         this.productId = product.getProductId();
         this.name = product.getName();
         this.originPrice = product.getOriginPrice();
         this.listId = product.getLists().getListId();
+        this.deletedYn = product.getDeletedYn();
+        this.createdAt = product.getCreatedAt();
     }
 }
