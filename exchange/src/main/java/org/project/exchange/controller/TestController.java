5a3a7626d8c774a1996a935f1b9e57cbdf9da1ca
jieun song <0scarl1216@mju.ac.kr>
2025-05-12 19:06:23 +0900

    test: add testController, applcation-prod.properties

--- exchange/src/main/java/org/project/exchange/controller/TestController.java
+++ exchange/src/main/java/org/project/exchange/controller/TestController.java
@@ -1,12 +1,14 @@
 package org.project.exchange.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @GetMapping("/")
+    @RequestMapping("/")
     public String test() {
         return "hi";
     }
 }
+
