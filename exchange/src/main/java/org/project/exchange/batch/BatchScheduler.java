261dcbcb8788a9b70af680f21b81bf09b0be8e95
chaewon02 <glgl246@naver.com>
2025-05-26 16:07:38 +0900

    feat: 휴일 배치 핸들링

--- exchange/src/main/java/org/project/exchange/batch/BatchScheduler.java
+++ exchange/src/main/java/org/project/exchange/batch/BatchScheduler.java
@@ -11,11 +11,6 @@ import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 
-import java.time.Duration;
-import java.time.LocalTime;
-import java.util.Timer;
-import java.util.TimerTask;
-
 @Slf4j
 @RequiredArgsConstructor
 @Component
@@ -24,45 +19,32 @@ public class BatchScheduler {
 	private final JobLauncher jobLauncher;
 	private final Job fetchCurrencyJob;
 
-	//@Scheduled(cron = "0 0 9 * * MON-FRI") // 평일 오전 9시 실행
-	//@Scheduled(cron = "0 27 15 * * MON-FRI")
-//	@Scheduled(cron = "*/30 * * * * *")
-//	public void runFetchCurrencyJob() {
-//		try {
-//			log.info("🔥 runFetchCurrencyJob 실행됨");
-//			JobExecution execution = jobLauncher.run(fetchCurrencyJob, new JobParametersBuilder()
-//					.addLong("time", System.currentTimeMillis())
-//					.toJobParameters());
-//			log.info("🔥 JobExecution: {}", execution);
-//		} catch (Exception e) {
-//			log.error("환율 데이터 수집 작업 실행 중 오류 발생", e);
-//		}
-//	}
-
 	private boolean jobSucceeded = false;
 
-	 @Scheduled(cron = "*/50 * * * * MON-FRI") // 30초마다 실행
+	 //@Scheduled(cron = "0 0 9 * * MON-FRI") // 평일 오전 9시 실행
+	 //@Scheduled(cron = "*/50 * * * * MON-FRI") // 30초마다 실행
+	 @Scheduled(cron = "*/50 * 16 * * MON-FRI")
 	 public void runFetchCurrencyJob() {
 	 	if (jobSucceeded) {
 	 		return;
 	 	}
 
 	 	try {
-	 		log.info("🔥 runFetchCurrencyJob 실행됨");
+	 		log.info("runFetchCurrencyJob 실행됨");
 	 		JobExecution execution = jobLauncher.run(fetchCurrencyJob, new JobParametersBuilder()
 	 				.addLong("time", System.currentTimeMillis())
 	 				.toJobParameters());
 
-	 		log.info("🔥 JobExecution: {}", execution);
+	 		log.info("JobExecution: {}", execution);
 
 	 		// 성공하면 플래그 true로 바꿔서 이후 실행 차단
 	 		if (execution.getStatus() == BatchStatus.COMPLETED) {
 	 			jobSucceeded = true;
-	 			log.info("✅ 환율 수집 배치 성공! 이후부터 실행 중단됨.");
+	 			log.info("환율 수집 배치 성공! 이후부터 실행 중단됨.");
 	 		}
 
 	 	} catch (Exception e) {
-	 		log.error("❌ 환율 데이터 수집 작업 실행 중 오류 발생", e);
+	 		log.error("환율 데이터 수집 작업 실행 중 오류 발생", e);
 	 	}
 	 }
 }
