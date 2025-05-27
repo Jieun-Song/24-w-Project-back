4f7a51f53cd53b1e43a99e759b807b1c11afd109
chaewon02 <glgl246@naver.com>
2025-05-28 04:03:35 +0900

    feat: response 수정 - 4

--- exchange/src/main/java/org/project/exchange/model/list/Dto/CreateListResponseDto.java
+++ exchange/src/main/java/org/project/exchange/model/list/Dto/CreateListResponseDto.java
@@ -7,7 +7,6 @@ import org.project.exchange.model.currency.Currency;
 import org.project.exchange.model.list.Lists;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Getter
 @NoArgsConstructor
@@ -16,7 +15,7 @@ public class CreateListResponseDto {
     private Long listId;
     private String name;
     private Long userId;
-    private String createdAt;
+    private LocalDateTime createdAt;
     private Currency currencyFrom;
     private Currency currencyTo;
     private String location;
@@ -26,7 +25,7 @@ public class CreateListResponseDto {
         this.name = lists.getName();
         this.userId = lists.getUser().getUserId();
         this.location = lists.getLocation();
-        this.createdAt = lists.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
+        this.createdAt = lists.getCreatedAt();
         this.currencyFrom = lists.getCurrencyFrom();
         this.currencyTo = lists.getCurrencyTo();
     }
