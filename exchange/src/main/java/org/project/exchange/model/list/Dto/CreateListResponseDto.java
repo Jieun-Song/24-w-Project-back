095498cd36df9aafb5a93a1f934c229b9e94fea3
chaewon02 <glgl246@naver.com>
2025-05-28 00:13:50 +0900

    fix: 충돌

--- exchange/src/main/java/org/project/exchange/model/list/Dto/CreateListResponseDto.java
+++ exchange/src/main/java/org/project/exchange/model/list/Dto/CreateListResponseDto.java
@@ -16,7 +16,7 @@ public class CreateListResponseDto {
     private Long listId;
     private String name;
     private Long userId;
-    private LocalDateTime createdAt;
+    private String createdAt;
     private Currency currencyFrom;
     private Currency currencyTo;
     private String location;
@@ -26,7 +26,7 @@ public class CreateListResponseDto {
         this.name = lists.getName();
         this.userId = lists.getUser().getUserId();
         this.location = lists.getLocation();
-        this.createdAt = lists.getCreatedAt() ;
+        this.createdAt = lists.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"));
         this.currencyFrom = lists.getCurrencyFrom();
         this.currencyTo = lists.getCurrencyTo();
     }
